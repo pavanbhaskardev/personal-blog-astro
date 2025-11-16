97ce921679ca1e86d088cd3791ab8fc1f9ffd96b
pavanbhaskar <pavanbhaskardev@gmail.com>
2025-11-16 15:38:06 +0530

    feat: new blog docker 1st step to self host

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -38,27 +38,33 @@ const Message = ({
   return (
     <div
       data-direction={direction}
-      className="mt-10 flex w-full gap-2 data-[direction=right]:flex-row-reverse"
+      className="mt-10 flex w-full flex-col gap-2"
     >
-      <img
-        className="not-prose size-12 flex-shrink-0 rounded-full bg-slate-300 object-cover"
-        src={imageURL}
-        alt={`${name} profile-pic`}
-        height={50}
-        width={50}
-        loading="lazy"
-      />
+      <div
+        data-direction={direction}
+        className="w-max flex-shrink-0 data-[direction=right]:self-end"
+      >
+        <img
+          className="not-prose size-12 flex-shrink-0 rounded-full bg-slate-300 object-cover"
+          src={imageURL}
+          alt={`${name} profile-pic`}
+          height={50}
+          width={50}
+          loading="lazy"
+        />
 
-      <div className="overflow-hidden">
         <p
           data-direction={direction}
           className="not-prose m-0 text-sm text-gray-500 data-[direction=right]:text-right"
         >
           {name}
         </p>
-        <div className="w-full max-w-2xl rounded-md bg-primary/20 p-4 [&>*]:!mt-0">
-          {children}
-        </div>
+      </div>
+
+      <div
+        className={`w-full max-w-2xl overflow-hidden rounded-xl bg-primary/20 p-4 [&>*]:!mt-0 ${direction === "right" ? "self-end rounded-tr-none" : "rounded-tl-none"}`}
+      >
+        {children}
       </div>
     </div>
   );
